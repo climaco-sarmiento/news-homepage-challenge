1b71e93d6d63aa03cc114b96395536de4f2eb87d
climaco-sarmiento <118277981+climaco-sarmiento@users.noreply.github.com>
2022-11-21 17:46:36 +0000

    Final solution, my first implementation of CSS Grid

---
+++ scripts.js
@@ -0,0 +1,39 @@
+
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "70%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+
+
+
+/*
+
+var right=document.getElementById('mobile-3').style.height;
+var left=document.getElementById('mobile-2').style.height;
+if(left>right)
+{
+    document.getElementById('mobile-3').style.height=left;
+}
+else
+{
+    document.getElementById('mobile-2').style.height=right;
+}
+
+*/
+
+/*
+var rightHeight = document.getElementById('mobile-3').clientHeight;
+var leftHeight = document.getElementById('mobile-2').clientHeight;
+if (leftHeight > rightHeight) {
+document.getElementById('mobile-3').style.height=leftHeight+'px';
+} else {
+document.getElementById('mobile-2').style.height=rightHeight+'px';
+}
+
+*/
